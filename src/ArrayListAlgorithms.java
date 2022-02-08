f758331ab2c40a458893b45746d034ba67c18230
BT_1E10_23 <sample@bths.edu>
2022-02-08 12:30:30 -0500

    Commit Message

--- src/ArrayListAlgorithms.java
+++ src/ArrayListAlgorithms.java
@@ -294,4 +294,48 @@ public class ArrayListAlgorithms {
             }
         }
     }
+
+    /** Returns Arraylist of Integers that contain all mode(s) of the int array numList.
+     *  If elements in numList all appear exactly once, there is no mode, and this method
+     *  should return an empty list
+     *
+     *  For example, if numList is: [1, 2, 3, 2, 4, 5, 5, 6],
+     *  then numList contains two modes: 2, 5
+     *  and this method returns an arraylist containing 2 and 5 (in any order)
+     *  If numList is: [1, 2, 3, 2, 4, 5, 5, 6, 6, 7, 6],
+     *  then numList contains one mode: 6
+     *  and this method returns an arrayList containing 6
+     *  If numList is: [1, 2, 3, 4, 5, 6], then numList contains no mode
+     *  and this method returns an empty arrayList: []
+     *  If numList is: [2, 2, 2, 3, 3, 3, 4, 4, 4],
+     *  then numList contains three modes: 2, 3, 4
+     *  and this method returns an arrayList containing 2, 3, and 4 (in any order)
+     *
+     *  Does NOT mutate (modify) elements in numList
+     *  PRECONDITIONS: numList.length > 0
+     *
+     *  @param numList  numList of ints
+     */
+    public static ArrayList<Integer> modes(int[] numList)
+    {
+        int count = 0;
+        ArrayList<Integer> modes = new ArrayList<Integer>();
+        for (int i = 0; i < numList.length; i++)
+        {
+            count = 0;
+            for (int j = i; j < numList.length; j++)
+            {
+                if (numList[j] == numList[i]) {
+                    count++;
+                }
+            }
+            if (count > modes.get(0)) {
+                modes.set(0, numList[i]);
+            }
+            for (int k = 0; k < modes.size(); k++) {
+
+            }
+        }
+        return modes;
+    }
 }
